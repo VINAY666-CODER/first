dbf36c3769034633c2f8053fa7c715297ce6623d
VINAY666-CODER <vinaygosula78@gmail.com>
2025-08-06 21:13:48 +0530

    Update if else.py

--- if else.py
+++ if else.py
@@ -1,7 +1,8 @@
-a=(int(input("enter the age :")))
+a=6;
 if(a>18):
     print("you are eligiblr for vote")
 elif(a==0):
     print("you are not born ")   
 else:
+
     print("you are not eligible for vote")    
